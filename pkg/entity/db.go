f422e456da27152cfb66eb7fa99baa98f7a28a88
serkanyazici <serkanyazici90@gmail.com>
2022-04-03 14:06:58 +0400

    revert back to original

--- pkg/entity/db.go
+++ pkg/entity/db.go
@@ -1,8 +1,5 @@
 package entity
 
-import (
-	"github.com/caarlos0/env/v6"
-)
 // NodeBinaries paths to scylladb executables on database node
 type NodeBinaries struct {
 	Cqlsh    string
@@ -11,6 +8,6 @@ type NodeBinaries struct {
 
 // Credentials scylladb credentials
 type Credentials struct {
-	User     string `env:"DB_USER"`
-	Password string `env:"DB_PASS"`
+	User     string
+	Password string
 }
